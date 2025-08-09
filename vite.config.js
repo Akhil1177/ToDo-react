1d6f3b24f284d3f9ef7b4a41164ab5d92c582c9f
Akhil <akdeveloper1177@gmail.com>
2025-08-09 12:43:04 +0530

    do deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/ToDo-react/',
+  base: '/ToDo-react/', 
   plugins: [react()],
-})
-
+});
